ba4e69011f8b91aafa0a5dbbcd0d1cbb0fca371b
Scott S <scott@schlesier.ca>
2021-07-31 09:58:24 -0700

    Revert "exploring record ids and substitute with finished nodes in sourceNodes"
    This reverts commit acfda886fda1f7f86c3e27874cb9b25966eeb78d.

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,14 +14,14 @@ function addImageToGallery(node, parentNode) {
   const gallery = galleries.get(name)
 
   if (gallery.thumbnail === undefined) {
-    gallery.thumbnail = node.id
+    gallery.thumbnail = node
   }
 
   if (gallery.images === undefined) {
     gallery.images = []
   }
 
-  gallery.images.push(node.id)
+  gallery.images.push(node)
 }
 
 exports.onCreateNode = ({ node, getNode }) => {
@@ -32,30 +32,10 @@ exports.onCreateNode = ({ node, getNode }) => {
   }
 }
 
-function injectNode(node, name, getNode) {
-  var linkedNode = getNode(node[name])
-  node[name] = linkedNode
-}
-
-function injectArrayOfNodes(node, name, getNode) {
-  var linkedNodes = node[name].map(id => getNode(id))
-  node[name] = linkedNodes
-}
-
-exports.sourceNodes = ({
-  actions,
-  createContentDigest,
-  createNodeId,
-  getNode,
-}) => {
+exports.sourceNodes = ({ actions, createContentDigest, createNodeId }) => {
   const { createNode } = actions
 
   for (const [_, gallery] of galleries) {
-    injectNode(gallery, "thumbnail", getNode)
-    injectArrayOfNodes(gallery, "images", getNode)
-
-    console.log(JSON.stringify(gallery, null, 4))
-
     createNode({
       ...gallery,
       id: createNodeId(gallery.parent),
