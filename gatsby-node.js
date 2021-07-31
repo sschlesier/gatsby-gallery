e7355e4971d812cc360bd008373f6d30b75cc5cd
Scott S <scott@schlesier.ca>
2021-07-30 20:04:49 -0700

    start move to building gallery model in onCreateNode and the sourcing it in sourceNodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,32 @@
+const galleries = [];
+
+function createGallery(node) {
+  const gallery = {
+    name: node.relativePath,
+    parent: node.id
+  }
+
+  galleries.push(gallery);
+}
+
 exports.onCreateNode = ({ node, getNode, actions, createNodeId, createContentDigest }) => {
-    const { createNode, createNodeField } = actions;
-    if (node.internal.type === 'ImageSharp') {
-      const parent = getNode(node.parent);
+  if (node.internal.type === 'Directory' && node.relativePath != "") {
+    createGallery(node);
+  }
+}
 
-      const gallery = {
-          name: parent.relativeDirectory,
-          thumbnail: node,
-          images: [],
-      }
+exports.sourceNodes = ({ actions, createContentDigest, createNodeId }) => {
+  const { createNode } = actions;
+  console.log('creating galleries: ' + galleries.length); //todo use the info log level
 
-      // createNodeField({
-      //   node,
-      //   name: 'gallery',
-      //   value: gallery,
-      // });
-
-      createNode({
-        ...gallery,
-        id: createNodeId(parent.id),
-        internal: {
-          type: 'gallery',
-          contentDigest: createContentDigest(gallery),
-        },
-      })
-    }
+  galleries.forEach(gallery =>
+    createNode({
+      ...gallery,
+      id: createNodeId(gallery.parent),
+      internal: {
+        type: 'gallery',
+        contentDigest: createContentDigest(gallery),
+      },
+    })
+  );
 }
