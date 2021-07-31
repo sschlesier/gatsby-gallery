acfda886fda1f7f86c3e27874cb9b25966eeb78d
Scott S <scott@schlesier.ca>
2021-07-31 09:46:09 -0700

    exploring record ids and substitute with finished nodes in sourceNodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,61 @@
-const galleries = new Map();
+const galleries = new Map()
 
 function createGallery(node) {
   const gallery = {
     name: node.relativePath,
-    parent: node.id
-  };
+    parent: node.id,
+  }
 
-  galleries.set(gallery.name, gallery);
+  galleries.set(gallery.name, gallery)
 }
 
 function addImageToGallery(node, parentNode) {
-  const name = parentNode.relativeDirectory;
-  const gallery = galleries.get(name);
+  const name = parentNode.relativeDirectory
+  const gallery = galleries.get(name)
 
-  if( gallery.thumbnail === undefined ) {
-    gallery.thumbnail = node;
+  if (gallery.thumbnail === undefined) {
+    gallery.thumbnail = node.id
   }
 
-  if( gallery.images === undefined ) {
-    gallery.images = [];
+  if (gallery.images === undefined) {
+    gallery.images = []
   }
 
-  gallery.images.push(node);
+  gallery.images.push(node.id)
 }
 
-exports.onCreateNode = ({ node, getNode, actions, createNodeId, createContentDigest }) => {
-  if (node.internal.type === 'Directory' && node.relativePath != "") {
-    createGallery(node);
-  }
-  else if ( node.internal.type === 'ImageSharp') {
+exports.onCreateNode = ({ node, getNode }) => {
+  if (node.internal.type === "Directory" && node.relativePath != "") {
+    createGallery(node)
+  } else if (node.internal.type === "ImageSharp") {
     addImageToGallery(node, getNode(node.parent))
   }
 }
 
-exports.sourceNodes = ({ actions, createContentDigest, createNodeId }) => {
-  const { createNode } = actions;
+function injectNode(node, name, getNode) {
+  var linkedNode = getNode(node[name]);
+  node[name] = linkedNode;
+}
+
+function injectArrayOfNodes(node, name, getNode) {
+  var linkedNodes = node[name].map( id => getNode(id));
+  node[name] = linkedNodes;
+}
+
+exports.sourceNodes = ({ actions, createContentDigest, createNodeId, getNode }) => {
+  const { createNode } = actions
 
   for (const [_, gallery] of galleries) {
+    injectNode(gallery, 'thumbnail', getNode);
+    injectArrayOfNodes(gallery, 'images', getNode);
+
+    console.log(JSON.stringify(gallery, null, 4))
+
     createNode({
       ...gallery,
       id: createNodeId(gallery.parent),
       internal: {
-        type: 'gallery',
+        type: "gallery",
         contentDigest: createContentDigest(gallery),
       },
     })
